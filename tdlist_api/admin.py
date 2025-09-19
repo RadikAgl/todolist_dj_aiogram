968c9826888279a30d8c1d1982efcaba07236bb4
Radik Agliamov <aglyam92@mail.ru>
2025-09-19 15:20:38 +0300

    telegram user model added to admin panel

--- tdlist_api/admin.py
+++ tdlist_api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Task, Category
+from .models import Task, Category, TGUser
 
 
 @admin.register(Task)
@@ -11,5 +11,11 @@ class TaskAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """Настройки отображения категорий в интерфейсе администратора"""
+    pass
+
+
+@admin.register(TGUser)
+class TGUserAdmin(admin.ModelAdmin):
     """Настройки отображения категорий в интерфейсе администратора"""
     pass
